b20e3d45a57aa6408fb90fdca06bec815765cec7
CKPRO520 <87696835+CKPRO520@users.noreply.github.com>
2025-02-14 04:17:09 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+     appId: "wx5b53aeff01acf10c",
+     appSecret: "b5c3af0b9e62fbd507470dadfbc61d08",
+     token: "DEEPSEEK2024", // 这个稍后微信公众号后台要用到
+     encodingAESKey: "k7YqL9ZvW1wB4nR8xTfCdS6mU3oP5iA0",
+     deepseekApiKey: "sk-902678025b6044dab846370a3d89f395",
+     sessions: new Map() // 用于存储用户对话
+   };
