633ad69f62406616a3d3cdf281f769821f133d60
CKPRO520 <87696835+CKPRO520@users.noreply.github.com>
2025-02-14 04:59:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-组件.出口 = {
-  appId: " wx5b53aeff01acf10c ",
-  应用秘密: " b 5c 3 af 0 b 9 e 62 FBD 507470 dad FBC 61d 08 ",
-  代币: " DEEPSEEK2024 ",
-  编码密钥: " k 7y QL 9 zvw 1 WB 4 NR 8 XT fcds 6 mu 3 op 5 ia 0 ",
-  deepseekApiKey: " sk-902678025 b 6044 dab 846370 a3 d 89 f 395 ",
+module.exports = {
+  appId: "wx5b53aeff01acf10c",
+  appSecret: "b5c3af0b9e62fbd507470dadfbc61d08",
+  token: "DEEPSEEK2024",
+  encodingAESKey: "SLOgl7zQRNhxfMuHxYKFrmW9KYTW8vbaGNMF4jIfasd",
+  deepseekApiKey: "sk-902678025b6044dab846370a3d89f395",
   // 改成嵌套结构存储对话
-  会议: 新的 地图() // 结构：用户ID → { current: 当前对话ID, history: {对话ID: 对话记录} }
+  sessions: new Map() // 结构：用户ID → { current: 当前对话ID, history: {对话ID: 对话记录} }
 };
